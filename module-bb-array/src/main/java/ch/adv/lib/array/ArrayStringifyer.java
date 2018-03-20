c4d018002127581b4fcc73f76b04f917395911f1
Michael Wieland <michi.wieland@hotmail.com>
2018-03-20 17:55:38 +0100

    include static fields during gson serialization

--- module-bb-array/src/main/java/ch/adv/lib/array/ArrayStringifyer.java
+++ module-bb-array/src/main/java/ch/adv/lib/array/ArrayStringifyer.java
@@ -13,11 +13,19 @@ import org.slf4j.LoggerFactory;
  * Builds a json string from the input session. Can only handle array sessions.
  */
 class ArrayStringifyer implements Stringifyer {
-    private Gson gson;
+
+    // TODO Builder should be stored and retrieved from the core!
+    private final GsonBuilder minifiedBuilder;
+    private final GsonBuilder prettyBuilder;
+
     private static final Logger logger = LoggerFactory.getLogger(SocketConnector.class);
 
     public ArrayStringifyer() {
-        gson = new Gson();
+        this.prettyBuilder = new GsonBuilder();
+        this.prettyBuilder.setPrettyPrinting();
+
+        this.minifiedBuilder = new GsonBuilder();
+        this.minifiedBuilder.excludeFieldsWithModifiers(java.lang.reflect.Modifier.TRANSIENT);
     }
 
     /**
@@ -29,8 +37,8 @@ class ArrayStringifyer implements Stringifyer {
     @Override
     public String stringify(Session session) {
         if (session.getModule().equals("array")) {
-            logger.debug("resulting json: " + new GsonBuilder().setPrettyPrinting().create().toJson(session));
-            return gson.toJson(session);
+            logger.debug("resulting json: " + prettyBuilder.create().toJson(session));
+            return minifiedBuilder.create().toJson(session);
         } else {
             logger.error("Wrong session for this Stringifyer. Module name is {} but should be 'array'", session.getSessionName());
             return null;
