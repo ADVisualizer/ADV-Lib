8e91e22060ab1ce0cd2bbb46791682203e3b50a0
Murièle Trentini <muriele@trentini.ch>
2018-03-27 16:20:41 +0200

    99: add javadoc

--- lib-core/src/main/java/ch/adv/lib/logic/model/Snapshot.java
+++ lib-core/src/main/java/ch/adv/lib/logic/model/Snapshot.java
@@ -35,10 +35,24 @@ public class Snapshot {
         this.snapshotDescription = snapshotDescription;
     }
 
+    /**
+     * Adds an element to the snapshot. Every module offers one or more
+     * specific elements, which describe the content of the snapshot. An
+     * element could for example be a node in a graph.
+     *
+     * @param element to be added
+     */
     public void addElement(ADVElement<?> element) {
         elements.add(element);
     }
 
+    /**
+     * Adds a relation between two {@link ADVElement}s. A relation can show an
+     * ordering between elements as in elemnts of an array or an actual
+     * connection like an edge in a graph.
+     *
+     * @param relation between two elements
+     */
     public void addRelation(ADVRelation relation) {
         relations.add(relation);
     }
