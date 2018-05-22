42cbe11d8418bbec5375e923a05b163d42bb8da0
Murièle Trentini <muriele@trentini.ch>
2018-05-22 11:12:10 +0200

    222: add queue module

---
+++ module-bb-queue/src/test/java/ch/hsr/adv/lib/queue/logic/QueueBuilderTest.java
@@ -0,0 +1,56 @@
+package ch.hsr.adv.lib.queue.logic;
+
+import ch.hsr.adv.commons.core.logic.domain.ADVElement;
+import ch.hsr.adv.commons.core.logic.domain.ModuleGroup;
+import ch.hsr.adv.lib.queue.logic.domain.QueueTestModule;
+import ch.hsr.adv.lib.queue.logic.domain.TestConstants;
+import com.google.inject.Inject;
+import org.jukito.JukitoRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.*;
+@RunWith(JukitoRunner.class)
+public class QueueBuilderTest {
+
+    @Inject
+    private QueueBuilder sut;
+
+
+    @Test
+    public void createModuleGroupTest() {
+        // GIVEN
+        QueueTestModule queueModule = new QueueTestModule();
+
+        // WHEN
+        ModuleGroup queueGroup = sut.build(queueModule);
+
+        // THEN
+        assertNotNull(queueGroup);
+    }
+
+    @Test
+    public void elementBuildTest() {
+        // GIVEN
+        QueueTestModule queueModule = new QueueTestModule();
+
+        // WHEN
+        ModuleGroup qeueueGroup = sut.build(queueModule);
+
+        // THEN
+        assertEquals(1, qeueueGroup.getElements().size());
+
+        ADVElement element = qeueueGroup.getElements().get(0);
+        assertEquals(0, element.getId());
+        assertEquals(TestConstants.ELEMENT_NAME, element.getContent());
+        assertNotNull(element.getStyle());
+
+        List<ADVElement> fixedElements = qeueueGroup.getElements().stream()
+                .filter(e -> e.getFixedPosX() != 0 || e.getFixedPosY() != 0)
+                .collect(Collectors.toList());
+        assertEquals(0, fixedElements.size());
+    }
+}
---
+++ module-bb-queue/src/test/java/ch/hsr/adv/lib/queue/logic/domain/TestConstants.java
@@ -0,0 +1,9 @@
+package ch.hsr.adv.lib.queue.logic.domain;
+
+
+
+public class TestConstants {
+
+    public static final String ELEMENT_NAME = "firstElement";
+    public static final String SESSION_NAME = "testSession";
+}
