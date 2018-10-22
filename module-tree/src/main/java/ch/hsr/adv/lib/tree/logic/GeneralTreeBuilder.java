9b69460894e67efac5f1e28c440ad160965f565d
Jan Winter <j4n.winter@gmail.com>
2018-10-22 15:23:31 +0200

    added cyclic check to general node builder

--- module-tree/src/main/java/ch/hsr/adv/lib/tree/logic/GeneralTreeBuilder.java
+++ module-tree/src/main/java/ch/hsr/adv/lib/tree/logic/GeneralTreeBuilder.java
@@ -8,11 +8,15 @@ import ch.hsr.adv.commons.tree.logic.domain.TreeNodeElement;
 import ch.hsr.adv.commons.tree.logic.domain.TreeNodeRelation;
 import ch.hsr.adv.lib.core.logic.ADVModule;
 import ch.hsr.adv.lib.core.logic.Builder;
+import ch.hsr.adv.lib.tree.logic.exception.CyclicNodeException;
 import ch.hsr.adv.lib.tree.logic.exception.RootUnspecifiedException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.inject.Singleton;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Builder Strategy for GeneralTreeModule. It builds a ModuleGroup
@@ -53,24 +57,29 @@ public class GeneralTreeBuilder implements Builder {
 
     private void buildNodes(ADVGeneralTreeNode<?> root,
                             ModuleGroup moduleGroup) {
+        Set<ADVGeneralTreeNode<?>> visitedNodes = new HashSet<>();
         logger.debug("Root Node: " + root.toString());
 
+        visitedNodes.add(root);
         moduleGroup.addElement(new TreeNodeElement(root, START_ID));
 
-        buildChildren(root, START_ID, moduleGroup);
+        buildChildren(root, START_ID, moduleGroup, visitedNodes);
     }
 
-    private long buildNode(ADVGeneralTreeNode<?> childNode, long parentId,
-                           long childId, ModuleGroup moduleGroup) {
+    private long buildChild(ADVGeneralTreeNode<?> childNode, long parentId,
+                            long childId, ModuleGroup moduleGroup,
+                            Set<ADVGeneralTreeNode<?>> visitedNodes) {
         if (childNode != null) {
             logger.debug("ChildNode: " + childNode.toString());
+
+            checkCyclicNode(visitedNodes, parentId, childNode);
 
             moduleGroup.addElement(new TreeNodeElement(childNode, childId));
 
             moduleGroup.addRelation(new TreeNodeRelation(parentId, childId,
                     childNode.getStyle()));
 
-            return buildChildren(childNode, childId, moduleGroup);
+            return buildChildren(childNode, childId, moduleGroup, visitedNodes);
         } else {
             logger.error("The childNode with ID " + childId + " and parentID "
                     + parentId + " is null");
@@ -82,18 +91,33 @@ public class GeneralTreeBuilder implements Builder {
     }
 
     private long buildChildren(ADVGeneralTreeNode<?> node, long nodeId,
-                               ModuleGroup moduleGroup) {
+                               ModuleGroup moduleGroup,
+                               Set<ADVGeneralTreeNode<?>> visitedNodes) {
         long lastChildId = nodeId;
 
         if (node.getChildren() != null) {
             for (int i = 0; i < node.getChildren().size(); i++) {
                 ADVGeneralTreeNode<?> childOfChild =
                         node.getChildren().get(i);
-                lastChildId = buildNode(childOfChild, nodeId,
-                        lastChildId + 1, moduleGroup);
+                lastChildId = buildChild(childOfChild, nodeId,
+                        lastChildId + 1, moduleGroup, visitedNodes);
             }
         }
 
         return lastChildId;
+    }
+
+    private void checkCyclicNode(Set<ADVGeneralTreeNode<?>> visitedNodes,
+                                 long parentRank,
+                                 ADVGeneralTreeNode<?> childNode) {
+        if (visitedNodes.contains(childNode)) {
+            String errorMessage = "the child (" + childNode.toString()
+                    + " of Parent with Rank " + parentRank + "is already a "
+                    + "node in the tree";
+            logger.error(errorMessage);
+            throw new CyclicNodeException(errorMessage);
+        } else {
+            visitedNodes.add(childNode);
+        }
     }
 }
