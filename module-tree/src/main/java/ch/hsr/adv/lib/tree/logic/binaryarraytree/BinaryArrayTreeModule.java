175178fa73278c29330f4e3b1e8e220fcc693936
Jan Winter <j4n.winter@gmail.com>
2018-10-26 15:38:38 +0200

    added base module component for BinaryArrayTreeModule

---
+++ module-tree/src/main/java/ch/hsr/adv/lib/tree/logic/binaryarraytree/BinaryArrayTreeModule.java
@@ -0,0 +1,66 @@
+package ch.hsr.adv.lib.tree.logic.binaryarraytree;
+
+import ch.hsr.adv.commons.tree.logic.ConstantsTree;
+import ch.hsr.adv.lib.array.logic.ArrayModule;
+import ch.hsr.adv.lib.core.logic.ADVModule;
+import ch.hsr.adv.lib.tree.logic.TreeModuleBase;
+import ch.hsr.adv.lib.tree.logic.exception.RootUnspecifiedException;
+
+import java.util.ArrayList;
+
+/**
+ * Encapsulates module meta data and a node array to be sent to the ADV UI.
+ * @param <T> type of the array elements
+ */
+public class BinaryArrayTreeModule<T> extends TreeModuleBase {
+
+    public BinaryArrayTreeModule(T[] nodeArray, String sessionName) {
+        super(sessionName);
+        setArray(nodeArray);
+    }
+
+    @SuppressWarnings("unchecked")
+    public BinaryArrayTreeModule(ArrayList<T> nodeList, String sessionName) {
+        super(sessionName);
+
+        T[] nodeArray = (T[]) nodeList.toArray();
+        setArray(nodeArray);
+    }
+
+    @Override
+    public String getModuleName() {
+        return ConstantsTree.MODULE_NAME_BINARY_ARRAY_TREE;
+    }
+
+    /**
+     * Adds child-Modules, which is important for the Array Visualization of
+     * a binary tree which needs the array-module
+     *
+     * @param module child module
+     */
+    public void addChildModule(ADVModule module) {
+        this.getChildModules().add(module);
+    }
+
+    /**
+     * In case the array should not be visible in the UI, the BinaryTreeBuilder
+     * has the possibility to remove the ArrayModule
+     */
+    void removeArrayModule() {
+        if (getChildModules().size() > 0) {
+            getChildModules().remove(0);
+        }
+    }
+
+    private boolean hasRoot(T[] nodeArray) {
+        return nodeArray.length >= 2 && nodeArray[1] != null;
+    }
+
+    private void setArray(T[] nodeArray) {
+        if (!hasRoot(nodeArray)) {
+            throw new RootUnspecifiedException("Root should not be null");
+        }
+
+        addChildModule(new ArrayModule(nodeArray));
+    }
+}
