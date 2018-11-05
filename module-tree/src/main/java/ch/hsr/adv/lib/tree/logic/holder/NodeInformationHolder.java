bcf6a01f0bcbac6230baf42f7b7f6a5d0eb85a72
Jan Winter <j4n.winter@gmail.com>
2018-11-05 12:24:25 +0100

    added type description according to code review 2

--- module-tree/src/main/java/ch/hsr/adv/lib/tree/logic/holder/NodeInformationHolder.java
+++ module-tree/src/main/java/ch/hsr/adv/lib/tree/logic/holder/NodeInformationHolder.java
@@ -5,7 +5,7 @@ import ch.hsr.adv.commons.tree.logic.domain.ADVTreeNode;
 /**
  * The NodeInformationHolder contains information which are important for the
  * tree traversal in the BinaryTreeBuilder class
- * @param <T>
+ * @param <T> Specification of the node type
  */
 public class NodeInformationHolder<T extends ADVTreeNode<?>> {
     private long parentRank;
